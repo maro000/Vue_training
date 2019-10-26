0312b062034fd1dc459003e75a75f10509444c99
takayoshi_N <takayoshi@takayoshi-NnoMacBook-Pro.local>
2019-10-26 18:39:33 +0900

    chapter2-10

--- chapter2 /main.js
+++ chapter2 /main.js
@@ -1,10 +1,17 @@
 var app = new Vue({
     el : '#app',
     data:{
+        ok:true,
         message:{
             value:'Vue.js!'
         },
+        count:0,
         list:['apple','banana','storoberry'],
         num:1
+    },
+    methods:{
+        increment: function(){
+            this.count +=1;
+        }
     }
 })
