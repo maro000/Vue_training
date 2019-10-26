2fd4b99cb8b76acd41f02780ad8a62e6c702909c
takayoshi_N <takayoshi@takayoshi-NnoMacBook-Pro.local>
2019-10-27 00:34:29 +0900

    chapter2

--- chapter2 /main.js
+++ chapter2 /main.js
@@ -1,17 +1,35 @@
 var app = new Vue({
     el : '#app',
     data:{
+        name:'キマイラ',
         ok:true,
         message:{
             value:'Vue.js!'
         },
         count:0,
-        list:['apple','banana','storoberry'],
+        list: [
+            {id : 1, name:'slime',hp: 100},
+            {id : 2, name:'goblin',hp: 200},
+            {id : 3, name:'dragon',hp: 500}
+        ],
         num:1
     },
     methods:{
         increment: function(){
             this.count +=1;
-        }
+        },
+        doAdd:function(){
+            var max=this.list.reduce(function(a,b,){
+                return a > b.id ? a : b.id
+            },0)
+            this.list.push({
+                id: max+1,
+                name:this.name,
+                hp:500
+            })
+        },
+        doRemove:function(index){
+            this.list.splice(index,1)
+        }   
     }
 })
